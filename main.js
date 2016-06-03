260f74b4bf37fd80d4096b3f8776f7181496f3d6
LapshinIV <ivan.lapshin@39parrot.net>
2016-06-03 16:02:11 +0400

    complete step 1

--- main.js
+++ main.js
@@ -1,52 +1,59 @@
 function removePassengersandBookingComponent() {
-  var passengers = document.getElementById('passengers');
-  var booking = document.getElementById('booking')
+  var passengers = document.getElementById('passengersName');
+  var booking = document.getElementById('bookingNumber')
   passengers.parentNode.removeChild(passengers);
   booking.parentNode.removeChild(booking);
 
 }
 
+
 function constructFlightNumber(){
-	var appendFlightNumbers = _.map(_.keys(listOfFlights), function(flightNumber){
+	var appendFlightNumbers = _.map(_.keys(listOfFlights), function(flightNumber){  //render in html
+		console.log(flightNumber)
 		var newFlightNumberDiv;
 		  {
 			newFlightNumberDiv = document.createElement('div')
 		    newFlightNumberDiv.id = flightNumber				
 		    newFlightNumberDiv.innerHTML = flightNumber
 			newFlightNumberDiv.onclick = function(){return onFlightClick(this)}
-			document.getElementById("flights").appendChild(newFlightNumberDiv)
+			document.getElementById("flightsNumbers").appendChild(newFlightNumberDiv) // по моему хрень... child in child?? стили..
 	      }
 	})
 }
+
 
 function createHeaderElementsOfTable(){
 	var newHeaderPassenger
 		{
 			newDivPassenger = document.createElement("div")
-			newDivPassenger.id = "passengers"	
+			newDivPassenger.id = "passengersName"
+			newDivPassenger.innerHTML = "Passenger Name"	
 		}
 	var newHeaderBooking
 		{
 			newDivBooking = document.createElement("div")
-			newDivBooking.id = "booking"	
+			newDivBooking.id = "bookingNumber"
+			newDivBooking.innerHTML = "Booking Number"	
 		}
 	document.body.appendChild(newDivPassenger)
 	document.body.appendChild(newDivBooking)
 }
 
 
+
 function constructBookingAndPassengers(listOfPassengers){
-	console.log(listOfPassengers)
+	console.log("listofPassengers:", listOfPassengers)
 	var appendPassengersList = _.map(listOfPassengers, function(passengers){
-		console.log(passengers)
 	var newPassDiv
 		{
 			newPassDiv = document.createElement("div")
+			newPassDiv.id = "passengers"
 			newPassDiv.innerHTML = passengers.name
 		}
 	var newBookDiv
 		{
 			newBookDiv = document.createElement("div")
+			newBookDiv.id = "booking"
 			newBookDiv.innerHTML = passengers.booking
 		}
 		
